efcca1534ed01259db427e16b29d4c541d65f55f
Sho Hashimoto <hashimoto@shokai.org>
2017-07-06 01:56:06 +0900

    実行すると現在時刻を追記するscript

---
+++ README.md
@@ -0,0 +1,10 @@
+# google-spreadsheet-study
+
+
+## install dependencies
+
+    $ npm i
+
+## setup service account
+
+![](https://gyazo.com/973adeb43690c70be286e20a5a885fdc/raw)
---
+++ add-date.js
@@ -0,0 +1,24 @@
+import GoogleSpreadsheetAsPromised from 'google-spreadsheet-as-promised'
+
+const CREDS = require('./creds.json')
+const SHEET_ID = '1Q9qh3rvFkkmrjeVvKCM4GtRNdd5QHNWkC2g4LlqmE4I';
+
+// シート末尾を探す
+async function getTailRowIndex (worksheet) {
+  const cells = (await worksheet.getCells('A1:A1500')).getAllValues()
+  console.log(cells.length)
+  for (let i = 0; i < cells.length; i++) {
+    if (cells[i] === '') return i
+  }
+}
+
+(async function () {
+  const sheet = new GoogleSpreadsheetAsPromised()
+  await sheet.load(SHEET_ID, CREDS)
+  const worksheet = await sheet.getWorksheetByName('sheet1') // 事前にsheet1というシートを作っておく必要がある
+  const tailRowIndex = await getTailRowIndex(worksheet)
+
+  const cell = await worksheet.getCell(`A${tailRowIndex + 1}`)
+  await cell.setValue(new Date().toString()) // 現在の日付を追記
+  console.log('done')
+})().catch(err => console.error(err.stack || err))
